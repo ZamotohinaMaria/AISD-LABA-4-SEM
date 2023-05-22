c28a82a507ff51ad60f079c989eef49c51331071
Маша Замотохина <zamotokhina.masha@mail.ru>
2023-05-22 22:36:03 +0400

    влаовлолв

--- AISD-LABA-3/exeption.cpp
+++ AISD-LABA-3/exeption.cpp
@@ -1,17 +1,17 @@
-//#include <stdio.h>
-//#include <iostream>
-//#include "exeption.h"
-//
-//using namespace std;
-//
-//void EZeroVertexesSize:: print_exeption() const
-//{
-//	cout << "Graph is empty" << endl;
-//}
-//
-//EElementNotExist::EElementNotExist(int id) : id(id) {};
-//
-//void EElementNotExist::print_exeption() const
-//{
-//	cout << "Vertex with id" << id << "not exist" << endl;
-//}
+#include <stdio.h>
+#include <iostream>
+#include "exeption.h"
+
+using namespace std;
+
+void EZeroVertexesSize:: print_exeption() const
+{
+	cout << "Graph is empty" << endl;
+}
+
+EElementNotExist::EElementNotExist(int id) : id(id) {};
+
+void EElementNotExist::print_exeption() const
+{
+	cout << "Vertex with id" << id << "not exist" << endl;
+}
